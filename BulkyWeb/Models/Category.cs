63f8baee95ae73573cfc0d90a51bfa694ccefdd3
Atikul1213 <atikul.islam@brainstation-23.com>
2024-05-08 11:47:19 +0600

    Add Validation

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -9,8 +9,10 @@ namespace BulkyWeb.Models
         public int Id { get; set; }
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(100)]
         public string Name { get; set; }
         [DisplayName("Diplay Order")]
+        [Range(1,100,ErrorMessage ="Value Must be 1 to 100")]
         public string DisplayOrder { get; set; }
 
     }
